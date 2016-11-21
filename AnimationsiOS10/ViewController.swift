c94344b7afdf36a21a0ce2196633dad866827c19
ellinakuznetcova <ellina.kuznetcova@flatstack.com>
2016-11-21 20:00:29 +0300

    init

--- AnimationsiOS10/ViewController.swift
+++ AnimationsiOS10/ViewController.swift
@@ -9,17 +9,58 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var squareView: UIView!
+    var squareCenter: CGPoint!
+    var squareAnimator: UIViewPropertyAnimator!
+    var comeBackAnimator: UIViewPropertyAnimator!
+    let animationDuration = 1.0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.squareView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(self.dragSquare)))
+        
+        self.squareAnimator = UIViewPropertyAnimator(duration: animationDuration, curve: .easeInOut, animations: { [weak self] in
+            self?.squareView.transform = CGAffineTransform(scaleX: 2.0, y: 2.0)
+        })
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func dragSquare(gesture: UIPanGestureRecognizer) {
+        let target = gesture.view!
+        
+        switch gesture.state {
+        case .began:
+            squareCenter = target.center
+            
+            if self.squareAnimator.state == .active {
+                self.squareAnimator.stopAnimation(true)
+            }
+            
+            self.squareAnimator.addAnimations({
+                target.transform = CGAffineTransform(scaleX: 2.0, y: 2.0)
+            })
+            self.squareAnimator.startAnimation()
+            // Three important properties on an animator:
+            print("Animator isRunning, isReversed, state: \(self.squareAnimator.isRunning), \(self.squareAnimator.isReversed), \(self.squareAnimator.state)")
+        case .changed:
+            let translation = gesture.translation(in: self.view)
+            target.center = CGPoint(x: squareCenter!.x + translation.x, y: squareCenter!.y + translation.y)
+        case .ended:
+            let v = gesture.velocity(in: target)
+            let velocity = CGVector(dx: v.x / 100, dy: v.y / 100)
+            let springParameters = UISpringTimingParameters(mass: 100, stiffness: 70, damping: 2, initialVelocity: velocity)
+            self.comeBackAnimator = UIViewPropertyAnimator(duration: 0.0, timingParameters: springParameters)
+            self.comeBackAnimator.addAnimations { [unowned self] in
+                target.center = self.view.center
+            }
+            self.squareAnimator.addAnimations({
+                target.transform = CGAffineTransform.identity
+            })
+            self.squareAnimator.startAnimation()
+            self.comeBackAnimator.startAnimation()
+        default: break
+        }
     }
-
-
 }
 
